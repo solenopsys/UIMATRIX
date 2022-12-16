c7f069e705b375a0954b35d9fd407afb10677f73
AlexStorm <info@alexstorm.ru>
2022-12-16 20:15:06 +0530

    fix start

---
+++ tools/libs-submodules.sh
@@ -0,0 +1,5 @@
+#git submodule add https://github.com/solenopsys/sc-fl-storage   packages/libs/storage/
+#git submodule add https://github.com/solenopsys/sc-fl-hyperstreams   packages/libs/hyperstreams/
+#git submodule add https://github.com/solenopsys/sc-fl-globals   packages/libs/globals/
+#git submodule add https://github.com/solenopsys/sc-fl-dgraph   packages/libs/dgraph/
+git submodule add https://github.com/solenopsys/sc-fl-clusters   packages/libs/clusters/
